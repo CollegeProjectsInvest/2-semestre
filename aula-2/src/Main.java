f723f79f3c05a8a6fab5481c0ed131c336f25e7c
Marcos Guilherme <marcos@MacBook-Air-de-Marcos.local>
2024-08-29 20:20:05 -0400

    feat: aula 2 - parte 1

---
+++ aula-2/src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        // Tipos de Dados Primitivos
+
+        // tipoDeDado nomeVariavel;
+        // tipoDeDado nomeVariavel = valor;
+
+        // byte umaVariavelByte = 100;
+        // double altura = 1.5;
+        // float peso = 1.9f;
+        // int idade = 19;
+        // Concatenar String
+        // System.out.println("Altura: " + altura);
+
+        boolean maiorDeIdade = true; // ou false
+        System.out.println("Maior Idade? " + maiorDeIdade);
+
+        String nome = "Marcos"; // é uma classe
+        System.out.println("Olá " + nome);
+
+        // char - único caractere
+        char sexo = 'M';
+
+        // tipagem dinâmica
+        var sobrenome = "Campos";
+
+        // constantes
+        final int IDADE = 10;
+        //IDADE = 10; // ERRO
+    }
+}
