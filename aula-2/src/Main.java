ef01839c54bf31925104802967556da53599154b
Marcos Guilherme <marcos@MacBook-Air-de-Marcos.local>
2024-08-29 20:53:50 -0400

    feat: aula 2 - parte 2

--- aula-2/src/Main.java
+++ aula-2/src/Main.java
@@ -8,24 +8,49 @@ public class Main {
         // byte umaVariavelByte = 100;
         // double altura = 1.5;
         // float peso = 1.9f;
+        // System.out.println(peso);
         // int idade = 19;
         // Concatenar String
         // System.out.println("Altura: " + altura);
+//
+//        boolean maiorDeIdade = true; // ou false
+//        System.out.println("Maior Idade? " + maiorDeIdade);
+//
+//        String nome = "Marcos"; // é uma classe
+//        System.out.println("Olá " + nome);
+//
+//        // char - único caractere
+//        char sexo = 'M';
+//
+//        // tipagem dinâmica
+//        var sobrenome = "Campos";
+//
+//        // constantes
+//        final int IDADE = 10;
+//        //IDADE = 10; // ERRO
 
-        boolean maiorDeIdade = true; // ou false
-        System.out.println("Maior Idade? " + maiorDeIdade);
+        /* Operadores */
+//        int numero = (10 + 10) - 5 * 2 / 1;
+//        System.out.println(numero);
+//        System.out.println(10 % 2);
 
-        String nome = "Marcos"; // é uma classe
-        System.out.println("Olá " + nome);
+//        int numero = 0;
+//        numero += 10; // numero = numero + 10;
+//        numero -= 1; // numero = numero - 1;
+//        numero++; // numero = numero + 1;
+//        numero--; // numero = numero - 1;
+//        ++numero;
+//        --numero;
 
-        // char - único caractere
-        char sexo = 'M';
-
-        // tipagem dinâmica
-        var sobrenome = "Campos";
-
-        // constantes
-        final int IDADE = 10;
-        //IDADE = 10; // ERRO
+        //boolean comparacao = 10 == 10; // true
+        //boolean comparacao = 10 > 10; // false
+        // boolean comparacao = 10 >= 10; // true
+        // boolean comparacao = 10 != 10; // false
+        // boolean negacao = !true; // false
+        // Comparar String
+        // boolean compararString = "123".equals("123"); // true
+        // boolean compararString = "abc".equalsIgnoreCase("ABC"); // true
+        // boolean logico = true && true; // true
+        // boolean logico = true || false || true || false; // true
     }
 }
