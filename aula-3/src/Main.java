e2908e9ba32f0f49ee91e2e5117f1a3b3e26ec2d
Markimg22 <marquim.net.221200@hotmail.com>
2024-09-12 20:13:25 -0400

    aula 3

---
+++ aula-3/src/Main.java
@@ -0,0 +1,75 @@
+public class Main {
+    public static void main(String[] args) {
+        // IF - Se
+//        int idade = 17;
+//        if (idade >= 18) {
+//            System.out.println("maior de idade");
+//        }
+        // If Else
+//        int idade = 17;
+//
+//        if (idade >= 18) {
+//            System.out.println("Maior de idade");
+//        } else {
+//            System.out.println("Menor de idade");
+//        }
+
+        // Else if
+        /*
+        int nota = 2;
+
+        if (nota >= 10) {
+            System.out.println("Aprovado");
+        } else if (nota < 4) {
+            System.out.println("Reprovado");
+        } else if (nota >= 4 && nota < 10) {
+            System.out.println("Recuperação");
+        }
+         */
+
+        // Switch = Escolha
+        // Valores absolutos
+//        int valor = 5;
+//        switch (valor) {
+//            case 0:
+//                System.out.println("Valor 0");
+//                break;
+//            case 1:
+//                System.out.println("Valor 1");
+//                break;
+//            case 2: case 3:
+//                System.out.println("Valor 2 ou 3");
+//                break;
+//            default:
+//                System.out.println("Valor invalido");
+//        }
+
+        // If Ternário
+//        int idade = 16;
+//        String resultado = idade >= 18 ? "maior idade" : "menor idade";
+//        System.out.println(resultado);
+
+        // Repetição:
+
+        // For = Para
+//        for (int i = 0; i <= 10; i+=2) {
+//            System.out.println(i);
+//        }
+
+        // While = Enquanto
+        int contador = 0;
+        while (contador <= 10) {
+            System.out.println(contador);
+            contador++;
+
+            if (contador == 2) break;
+        }
+//
+//        System.out.println("Loop pausado");
+
+        // Do while = Faça depois verifica
+//        do {
+//            System.out.println("Executou");
+//        } while (false);
+    }
+}
